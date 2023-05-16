dfd1e26a147b8a9a8a8a48c3999ae620c757afe6
imilay11 <imilay11@gmail.com>
2023-05-16 20:02:48 +0300

    Deleted import classnames

--- src/components/header/logo/Logo.tsx
+++ src/components/header/logo/Logo.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import { Button, Link, Stack } from '@mui/material'
 import { ArrowBack } from '@mui/icons-material'
 import { Coca, CocaText } from 'assets'
-import cn from 'classnames'
 import s from './Logo.module.scss'
 // interface LogoProps {
 // }
@@ -10,14 +9,14 @@ import s from './Logo.module.scss'
 const Logo: FC = () => {
   return (
     <Stack alignItems="center" direction="row" spacing={4.8}>
-      <Button variant="outlined" className={cn([s.buttonBack])}>
+      <Button variant="outlined" className={s.buttonBack}>
         <ArrowBack className={s.arrow} color="secondary" />
       </Button>
       <div className={s.line} />
       {/* here will be a Link routing */}
       <Link href="/" className={s.logo}>
         <Coca className={s.coca} />
-        <CocaText className={cn([s.cocaText])} />
+        <CocaText className={s.cocaText} />
       </Link>
     </Stack>
   )
