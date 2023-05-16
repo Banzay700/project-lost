9a5c9842c7e0c850e3499e9a273e054128667f42
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-05-16 18:09:50 +0300

    add fontsize

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Input } from 'UI/index'
+
 const App = () => {
-  return <div />
+  return (
+    <div>
+      <Input placeholder="test" label="test" />
+    </div>
+  )
 }
 
 export default App
