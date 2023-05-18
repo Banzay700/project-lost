ceba0bb4128b10a899d6f1450f02eff7ee056cc0
imilay11 <imilay11@gmail.com>
2023-05-18 20:51:27 +0300

    Deleted unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Header } from 'components'
-import { LogoWrapper, Watch, Navbar } from 'components/header'
-import { navData } from 'utils'
-
 const App = () => {
-  return (
-    <Router>
-      <Header className="header">
-        <LogoWrapper />
-        <Navbar spacing={6.4} direction="row" data={navData} />
-        <Watch />
-      </Header>
-    </Router>
-  )
+  return <div />
 }
 
 export default App
