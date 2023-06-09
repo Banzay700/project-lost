fb94f93621088b203b904e8710540f1ce41e1c6c
Julia <bullet.jul@gmail.com>
2023-06-10 01:23:19 +0300

    part of calendar+res conf

--- src/components/form-componets/create-dish-category-form/CreateDishCategoryForm.tsx
+++ src/components/form-componets/create-dish-category-form/CreateDishCategoryForm.tsx
@@ -4,9 +4,8 @@ import { Stack } from '@mui/material'
 import { Input } from 'UI/input'
 import { CreateCategoryPicture } from './create-category-picture'
 
-interface CreateDishCategoryFormProps {
-  '': any
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface CreateDishCategoryFormProps {}
 
 const CreateDishCategoryForm: FC<CreateDishCategoryFormProps> = () => {
   const handleSubmit = (value: any) => {
