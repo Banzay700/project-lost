35a0e334e6ad0f0f532811a5bfad6b253599cbcc
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-06-27 14:45:31 +0300

    hotFix

--- src/UI/input-phone-number/index.ts
+++ src/UI/input-phone-number/index.ts
@@ -1,1 +1,1 @@
-export { default as PhoneNumberInput } from './InputPhoneNumber'
+export { default as InputPhoneNumber } from './InputPhoneNumber'
--- src/components/delivery-components/info-delivery/InfoDelivery.styled.ts
+++ src/components/delivery-components/info-delivery/InfoDelivery.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { Stack } from '@mui/material'
 
-export const InfoDeliveryWrapper = styled(Stack)(({ theme }) => ({
+export const InfoDeliveryWrapper = styled(Stack)(() => ({
   padding: '20px',
   gap: '10px',
 }))
