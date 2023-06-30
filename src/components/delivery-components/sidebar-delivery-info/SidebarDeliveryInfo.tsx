b0d818817c6a52252dbc9c5fddc16391faf2916b
imilay11 <imilay11@gmail.com>
2023-06-30 20:41:07 +0300

    .

--- src/components/delivery-components/sidebar-delivery-info/SidebarDeliveryInfo.tsx
+++ src/components/delivery-components/sidebar-delivery-info/SidebarDeliveryInfo.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react'
-import { OrderDetailList, OrderSummaryWrapper } from 'components'
-import { OrderType } from 'types'
-import { DetailsListTitle } from 'UI/details-title'
 import { Stack } from '@mui/material'
+import { OrderDetailList, OrderSummaryWrapper } from 'components'
+import { Button, DetailsListTitle } from 'UI'
+import { OrderType } from 'types'
 import { TAX } from 'utils'
-import { Button } from 'UI'
 import {
   SidebarDeliveryActionsWrapper,
   SidebarDeliveryInfoWrapper,
