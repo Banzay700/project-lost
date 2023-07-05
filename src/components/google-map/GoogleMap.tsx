8d75d9100ffe1c5d82d81e8a849b9a5b4fcf41b7
Julia <bullet.jul@gmail.com>
2023-07-05 22:16:11 +0300

    add send notify frontend

--- src/components/google-map/GoogleMap.tsx
+++ src/components/google-map/GoogleMap.tsx
@@ -6,14 +6,14 @@ import { GoogleMapBox } from './GoogleMap.styled'
 import { GoogleRouteInfo } from './google-route-info'
 import { directionsServices } from './GoogleMap.utils'
 
-interface GoogleMapProps {
-  lat: number
-  lng: number
-  zoom?: number
-  hideTripSummary?: boolean
-  isMarker?: boolean
-  mapActions?: MapActionsType
-}
+// interface GoogleMapProps {
+//   lat: number
+//   lng: number
+//   zoom?: number
+//   hideTripSummary?: boolean
+//   isMarker?: boolean
+//   mapActions?: MapActionsType
+// }
 
 const GoogleMap: FC<GoogleMapProps> = ({
   lat,
@@ -23,60 +23,53 @@ const GoogleMap: FC<GoogleMapProps> = ({
   isMarker,
   mapActions,
 }) => {
-  const mapRef = useRef<HTMLDivElement | null>(null)
-  const [routeInfo, setRouteInfo] = useState<RouteInfoType>({
-    duration: '',
-    distance: '',
-  })
-
-  const initMap = async () => {
-    const { MapTypeControlStyle, Marker, Map, LatLng, TravelMode } = await window.google.maps
-    const position = { lat, lng }
-
-    const mapOptions: google.maps.MapOptions = {
-      center: position,
-      zoom: zoom || 14,
-      mapId: GOOGLE_MAP_ID_STYLE,
-      mapTypeControlOptions: {
-        style: MapTypeControlStyle.DROPDOWN_MENU,
-      },
-      ...mapActions,
-    }
-
-    const map = new Map(mapRef.current as HTMLElement, mapOptions)
-    if (isMarker) {
-      const marker = new Marker({
-        map,
-        position,
-        title: 'Destination',
-      })
-    } else {
-      const origin = new LatLng(ORIGIN_LAT, ORIGIN_LNG)
-      const destination = new LatLng(lat, lng)
-
-      const request: google.maps.DirectionsRequest = {
-        origin,
-        destination,
-        travelMode: TravelMode.DRIVING,
-      }
-
-      const directions = await directionsServices({ map, request })
-      setRouteInfo(directions)
-    }
-  }
-
-  useEffect(() => {
-    initMap()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-  return (
-    <Box sx={{ height: '100%', width: '100%', position: 'relative' }}>
-      <GoogleMapBox ref={mapRef} />
-      {!hideTripSummary && (
-        <GoogleRouteInfo duration={routeInfo.duration} distance={routeInfo.distance} />
-      )}
-    </Box>
-  )
+  // const mapRef = useRef<HTMLDivElement | null>(null)
+  // const [routeInfo, setRouteInfo] = useState<RouteInfoType>({
+  //   duration: '',
+  //   distance: '',
+  // })
+  // const initMap = async () => {
+  //   const { MapTypeControlStyle, Marker, Map, LatLng, TravelMode } = await window.google.maps
+  //   const position = { lat, lng }
+  //   const mapOptions: google.maps.MapOptions = {
+  //     center: position,
+  //     zoom: zoom || 14,
+  //     mapId: GOOGLE_MAP_ID_STYLE,
+  //     mapTypeControlOptions: {
+  //       style: MapTypeControlStyle.DROPDOWN_MENU,
+  //     },
+  //     ...mapActions,
+  //   }
+  //   const map = new Map(mapRef.current as HTMLElement, mapOptions)
+  //   if (isMarker) {
+  //     const marker = new Marker({
+  //       map,
+  //       position,
+  //       title: 'Destination',
+  //     })
+  //   } else {
+  //     const origin = new LatLng(ORIGIN_LAT, ORIGIN_LNG)
+  //     const destination = new LatLng(lat, lng)
+  //     const request: google.maps.DirectionsRequest = {
+  //       origin,
+  //       destination,
+  //       travelMode: TravelMode.DRIVING,
+  //     }
+  //     const directions = await directionsServices({ map, request })
+  //     setRouteInfo(directions)
+  //   }
+  // }
+  // useEffect(() => {
+  //   initMap()
+  // }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  // return (
+  //   <Box sx={{ height: '100%', width: '100%', position: 'relative' }}>
+  //     <GoogleMapBox ref={mapRef} />
+  //     {!hideTripSummary && (
+  //       <GoogleRouteInfo duration={routeInfo.duration} distance={routeInfo.distance} />
+  //     )}
+  //   </Box>
+  // )
 }
 
 export default GoogleMap
