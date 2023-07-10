422766bf67d33c91c21ea946753c00371041cebc
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-07-10 21:47:28 +0300

    fix statistic layout

--- src/pages/admin-pages/admin-statistics-page/statistics/Statistics.styled.ts
+++ src/pages/admin-pages/admin-statistics-page/statistics/Statistics.styled.ts
@@ -3,7 +3,8 @@ import { Stack, styled } from '@mui/material'
 export const StatisticsWrapper = styled(Stack)(({ theme }) => ({
   flex: 1,
   height: '100%',
-  backgroundColor: theme.palette.background.main,
   padding: '20px',
   gap: '15px',
+  justifyContent: 'center',
+  backgroundColor: theme.palette.background.main,
 }))
