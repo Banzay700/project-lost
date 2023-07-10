ae10ab4aa0ad101f24e29c3b585c4e752f1a3f96
Julia <bullet.jul@gmail.com>
2023-07-10 21:03:24 +0300

    fix adaptive styles

--- src/components/input-form/input-image/InputImage.styled.ts
+++ src/components/input-form/input-image/InputImage.styled.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import styled from 'styled-components'
 import { Box } from '@mui/material'
 import { theme } from 'theme'
